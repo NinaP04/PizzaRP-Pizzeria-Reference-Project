dfc6daa44bf9fcfec71b1bb249e8603cf83471e3
Nina Prihoda <nina.prihoda@students.fhnw.ch>
2025-11-21 18:25:19 +0100

    Refactor password reset with validation checks
    Updated password reset logic with validation checks for length, case, digits, and special characters. Improved user prompts for password requirements.

--- Password/Funktionen überarbeitet.py
+++ Password/Funktionen überarbeitet.py
@@ -25,23 +25,51 @@ while passwort != änderung_input:
     änderung_input = input("Versuchen Sie es erneut:")
     
 else:
-    print("")
-    print("Passwort neu festlegen")
-    print("")
+    print("\nPasswort neu festlegen\n")
     print("Ihr Passwort muss enthalten:")
-    print("-Mindestens 8 Zeichen")
-    print("-Gross- und Kleinschreibung")
-    print("-Mindestens eine Zahl")
-    print("-Mindestens ein Sonderzeichen")
-    print("")
+    print("- Mindestens 8 Zeichen")
+    print("- Gross- und Kleinschreibung")
+    print("- Mindestens eine Zahl")
+    print("- Mindestens ein Sonderzeichen: $, @, #, %, !, ?, &, *")
 
 
-# Neues Passwort bestätigen
-passwort_neu2 = (input("Geben Sie ihr neues Passwort erneut ein:"))
-print(" ")
+SpecialSym = ['$', '@', '#', '%', '!', '?', '&', '*']
+val = True
 
-if (passwort_neu1) == (passwort_neu2):
-    passwort = passwort_neu1
-    print("\n\033[92mPasswort erfolgreich geändert!\033[0m")
-else:
-    print("\n\033[31mDie Passwörter stimmen nicht überein!\033[0m")
+while True:
+    val = True
+    passwort_neu1 = input("Geben Sie Ihr neues Passwort ein: ")
+
+    
+    # Validierung
+    if len(passwort_neu1) < 8:
+        print('\n\033[31mPasswort muss mindestens 8 Zeichen lang sein\033[0m')
+        val = False
+    if len(passwort_neu1) > 20:
+        print('\n\033[31mPasswort darf maximal 20 Zeichen lang sein\033[0m')
+        val = False
+    if not any(char.isdigit() for char in passwort_neu1):
+        print('\n\033[31mPasswort muss mindestens eine Zahl enthalten\033[0m')
+        val = False
+    if not any(char.isupper() for char in passwort_neu1):
+        print('\n\033[31mPasswort muss mindestens einen Grossbuchstaben enthalten\033[0m')
+        val = False
+    if not any(char.islower() for char in passwort_neu1):
+        print('\n\033[31mPasswort muss mindestens einen Kleinbuchstaben enthalten\033[0m')
+        val = False
+    if not any(char in SpecialSym for char in passwort_neu1):
+        print('\n\033[31mPasswort muss mindestens ein Sonderzeichen enthalten ($,@,#,%,!,?,&,* )\033[0m')
+        val = False
+
+    
+    # Wenn gültig, zweite Eingabe abfragen
+    if val:
+        passwort_neu2 = input("Geben Sie Ihr neues Passwort erneut ein: ")
+        if passwort_neu1 == passwort_neu2:
+            passwort = passwort_neu1
+            print("\n\033[92mPasswort erfolgreich geändert!\033[0m")
+            break
+        else:
+            print("\n\033[31mDie Passwörter stimmen nicht überein!\033[0m\n")
+    else:
+        print("\n\033[31mBitte versuchen Sie es erneut.\033[0m\n")
