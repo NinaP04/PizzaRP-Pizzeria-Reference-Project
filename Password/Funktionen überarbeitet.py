a29da1f62a113548ba3c2e6a7138b8ab6e4f2f4d
Nina Prihoda <nina.prihoda@students.fhnw.ch>
2025-11-21 15:37:54 +0100

    Add password login functionality with attempt restriction

---
+++ Password/Funktionen überarbeitet.py
@@ -0,0 +1,19 @@
+import sys  
+
+#mit Passwort einloggen
+passwort = 1234
+anmeldeversuche = 0
+
+passwort_input = int(input("Bitte geben Sie ihr Passwort ein:"))
+while passwort != passwort_input:
+    anmeldeversuche = anmeldeversuche + 1
+
+    anmeldeversuche = int(anmeldeversuche)
+    if anmeldeversuche == 3:
+        print("\n\033[31mZu viele fehlerhafte Anmeldungen!\033[0m")
+        sys.exit(1) #evtl. ein Time-Out implementieren
+    else:
+        passwort_input = int(input("\n\033[31mFehlerhaftes Passwort! Versuchen Sie es nochmals:\033[0m"))
+else:
+    print("\n\033[92mErfolgreich eingeloggt!\033[0m")
+    print("")
